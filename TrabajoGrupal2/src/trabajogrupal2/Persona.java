ab463b634610c9baec4dfd839c853a324cf16dd4
antsecv14 <38107769+antsecv14@users.noreply.github.com>
2018-07-05 12:22:49 -0500

    Update Persona.java

--- TrabajoGrupal2/src/trabajogrupal2/Persona.java
+++ TrabajoGrupal2/src/trabajogrupal2/Persona.java
@@ -1,7 +1,7 @@
 package trabajogrupal2;
 
 public class Persona {
-
+// Declaramos variables 
     String nombre;
     String apellido;
     int edad;
@@ -9,11 +9,11 @@ public class Persona {
     double calificacion2;
 
     public void imprimir() {
-        System.out.println("\n-----DATOS ESTUDIANTE-----\n");
-        System.out.printf("%s %s\n", nombre, apellido);
-        System.out.printf("Edad - %d\n", edad);
-        System.out.printf("Calificacion 1 - %.2f\n", calificacion1);
-        System.out.printf("Calificacion 2 - %.2f\n", calificacion2);
-        System.out.printf("Promedio - %.2f\n", (calificacion1 + calificacion2) / 2);
+        System.out.println("\n-----DATOS ESTUDIANTE-----\n");//Encabezado del reporte
+        System.out.printf("%s %s\n", nombre, apellido);//Presentacion del nombre
+        System.out.printf("Edad - %d\n", edad);//Presentacion de la edad
+        System.out.printf("Calificacion 1 - %.2f\n", calificacion1);//Presentacion de la calificacion1
+        System.out.printf("Calificacion 2 - %.2f\n", calificacion2);//Presentacion de la calificacion2
+        System.out.printf("Promedio - %.2f\n", (calificacion1 + calificacion2) / 2);// Calculo y presentacion del promedio de calificaciones
     }
 }
